cb0dee6329979dc37ea1fd70d5ded0d8423e2e5d
Elliot Bouvier <elliotbouvier@protonmail.com>
2022-04-09 20:26:06 +0200

    feature: tests to ex06

---
+++ c00.h
@@ -0,0 +1,10 @@
+#ifndef __C00_H
+#define __C00_H
+#include "c00/ex00/ft_putchar.c"
+#include "c00/ex01/ft_print_alphabet.c"
+#include "c00/ex02/ft_print_reverse_alphabet.c"
+#include "c00/ex03/ft_print_numbers.c"
+#include "c00/ex04/ft_is_negative.c"
+#include "c00/ex05/ft_print_comb.c"
+#include "c00/ex06/ft_print_comb2.c"
+#endif
